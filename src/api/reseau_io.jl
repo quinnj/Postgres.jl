b583144aab3e343e8a568338e7de4b889ae4ac4e
Jacob Quinn <quinn.jacobd@gmail.com>
2026-03-18 15:39:56 -0600

    fix: adapt reseau buffered reads

--- src/api/reseau_io.jl
+++ src/api/reseau_io.jl
@@ -20,10 +20,22 @@ end
     return io.stop - io.next + 1
 end
 
+function _conn_read_some!(conn, buf::Vector{UInt8})::Int
+    return readbytes!(conn, buf, length(buf))
+end
+
+function _conn_read_some!(conn::Reseau.TCP.Conn, buf::Vector{UInt8})::Int
+    return Reseau.TCP._read_some!(conn, buf)
+end
+
+function _conn_read_some!(conn::Reseau.TLS.Conn, buf::Vector{UInt8})::Int
+    return Reseau.TLS._read_some!(conn, buf)
+end
+
 function fillbuffer!(io::BufferedConn)::Int
     io.eof_seen && return 0
     try
-        n = read!(io.conn, io.buf)
+        n = _conn_read_some!(io.conn, io.buf)
         io.next = 1
         io.stop = n
         if n == 0
