cf83706297b050579ed2298daf24b5d3ef65adda
Jacob Quinn <quinn.jacobd@gmail.com>
2026-05-26 06:54:34 -0600

    docs(ci): skip deploy on PR builds
    Keep Documenter deployment behind an explicit environment flag so pull request CI validates docs without requiring write access to gh-pages.

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,6 @@ using Documenter, Postgres
 
 makedocs(modules = [Postgres], sitename = "Postgres.jl")
 
-deploydocs(repo = "github.com/quinnj/Postgres.jl.git", push_preview = true)
+if get(ENV, "POSTGRES_DOCS_DEPLOY", "false") == "true"
+    deploydocs(repo = "github.com/quinnj/Postgres.jl.git", push_preview = true)
+end
